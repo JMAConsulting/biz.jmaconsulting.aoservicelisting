f544db0c777852872b2632231c13fc1f0ffceb94
Monish Deb <deb.monish@gmail.com>
2020-03-18 19:45:20 +0530

    AOSL-5 remove hardcoded custom keys from tpl

--- CRM/Aoservicelisting/Form/ProviderApplicationForm.php
+++ CRM/Aoservicelisting/Form/ProviderApplicationForm.php
@@ -117,6 +117,8 @@ class CRM_Aoservicelisting_Form_ProviderApplicationForm extends CRM_Aoservicelis
     $this->buildCustom(PRIMARY_PROFILE, 'profile');
     $this->buildCustom(SERVICELISTING_PROFILE1, 'profile1');
     $this->buildCustom(SERVICELISTING_PROFILE2, 'profile2');
+    $this->assign('REGULATED_SERVICE_CF', REGULATED_SERVICE_CF);
+    $this->assign('IS_REGULATED_SERVICE', IS_REGULATED_SERVICE);
 
     // this part is to render camp fields
     $customFields = civicrm_api3('CustomField', 'get', ['custom_group_id' => CAMP_CG])['values'];
